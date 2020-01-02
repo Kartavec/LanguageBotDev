c77fb58906bfb06efe2aac5d95763bdd99a309b7
kartavec <kartavec@gmail.com>
2020-01-02 15:27:12 +0300

    Мелкие доработк

---
+++ LanguageTutor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LanguageTutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tutor = new Tutor();
+            tutor.AddWord("hello", "привет");
+            tutor.AddWord("dog", "собака");
+            tutor.AddWord("cat", "кошка");
+            tutor.AddWord("byke", "велосипед");
+
+            while (true)
+            {
+                var word = tutor.GetRandomEngWord();
+                Console.WriteLine($"Как переводится слово: {word}");
+                var userAnswer = Console.ReadLine();
+                if (tutor.CheckWord(word, userAnswer))
+                    Console.WriteLine("Правильно!");
+                else
+                {
+                    var correctAnswer = tutor.Translate(word);
+                    Console.WriteLine($"Неверно. Правильный ответ: {correctAnswer}");
+                }
+            }
+        }
+    }
+}
